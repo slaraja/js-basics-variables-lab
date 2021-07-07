05fa263b6773f1acf5a72df08d4336cd3193e04d
Stephanie La Raja <stephanielaraja@gmail.com>
2021-07-07 08:10:48 -0400

    commit tree 84f007d8a2e713868a7c3b4c445932eab6817074

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// let companyName = 'Scuber'; 
+const companyName = 'Scuber'; 
+// var mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Chelsea';
+// var companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith'
+
+
